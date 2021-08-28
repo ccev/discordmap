3b25f2380614b266086939751eedfb3547f51ae8
ccev <42342921+ccev@users.noreply.github.com>
2021-08-28 02:11:55 +0200

    inc/dec width/height in settings

--- map/settings.py
+++ map/settings.py
@@ -15,6 +15,7 @@ class StyleSelect(discord.ui.Select):
                          max_values=1,
                          row=0)
         for i, (style_name, style_value) in enumerate(STYLES):
+            style_name = "Map Style: " + style_name
             self.options.append(discord.SelectOption(label=style_name, value=style_value, default=i == 0))
         self.dmap = dmap
 
@@ -42,3 +43,32 @@ class Settings(discord.ui.View):
 
     async def send(self, response: discord.InteractionResponse):
         await response.send_message(embed=self.embed, view=self, ephemeral=True)
+
+    async def _update_map(self, interaction):
+        await interaction.response.defer()
+        self.dmap.set_time()
+        await self.dmap.update()
+
+    async def __change_width(self, interaction, value):
+        self.dmap.width += value
+        await self._update_map(interaction)
+
+    async def __change_height(self, interaction, value):
+        self.dmap.height += value
+        await self._update_map(interaction)
+
+    @discord.ui.button(label="+ Width", row=2)
+    async def inc_width(self, _, interaction: discord.Interaction):
+        await self.__change_width(interaction, 50)
+
+    @discord.ui.button(label="- Width", row=2)
+    async def dec_width(self, _, interaction: discord.Interaction):
+        await self.__change_width(interaction, -50)
+
+    @discord.ui.button(label="+ Height", row=2)
+    async def inc_height(self, _, interaction: discord.Interaction):
+        await self.__change_height(interaction, 30)
+
+    @discord.ui.button(label="- Height", row=2)
+    async def dex_height(self, _, interaction: discord.Interaction):
+        await self.__change_height(interaction, -30)
